c1c321921d7f449bd99f5345973f6969d33bf24c
mahsa5 <100310712+mahsa5@users.noreply.github.com>
2022-03-02 11:10:36 +0330

    Add files via upload

---
+++ Exercise1/Question5.py
@@ -0,0 +1,6 @@
+#program to calcualte function of my_multiple(x, y)
+
+def my_mul(x, y):
+    multiply = x*y
+    return multiply
+print(my_mul(x=int(input("enter x: ")), y=int(input("enter y: "))))
